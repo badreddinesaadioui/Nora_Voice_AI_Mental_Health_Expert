22c8ede8042b13a91add21daec560655f922878a
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2024-08-29 09:38:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from transformation_functions import get_answer, text_to_speech, autoplay_audio, speech_to_text
 from audio_recorder_streamlit import audio_recorder
 from streamlit_float import *
-from langchain import OpenAI
+from langchain_community.llms import OpenAI
 import tempfile
 import openai
 
